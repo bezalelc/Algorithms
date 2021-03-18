3fdace80241cfb77c019722f5f1f3aa066b7d0bb
bb <bezalelcohen1@gmail.com>
2021-03-19 01:23:07 +0200

    temp version of multiply polynomials

--- fft/polinom.py
+++ fft/polinom.py
@@ -1,5 +1,4 @@
 import numpy as np
-import sympy as sp
 
 
 def eval_(A, x_0):
@@ -19,37 +18,76 @@ def eval_(A, x_0):
     return res
 
 
-def f(A, B):
+def mult_(A, B):
     """
-    convert represent Polynomials
+    multiply to Polynomials that represent by vector of values
+        for example: Polynomial A= 1-2x+3x^2 => A=[1,-2,3]
+                     Polynomial B= -4+2x     => B=[-4,2]
+                     Polynomial C= A*B= -4+10x-16x^2+6^3 and the return will be: [ -4  10 -16   6]
 
-    @param:
+    :param:
         @A,B vectors that represent Polynomials
 
-    :complexity: O(n^3)
+    :return:
+        vector of values that represent Polynomial C= A*B
+
+    :complexity: O(n^2)
     """
-    x = np.array([-1, 0, 1, 2])
+    # x = np.random.randint(low=-len(A) * 8, high=len(A) + len(B), size=len(A) + len(B) - 1)
+    x = np.arange(-len(A), len(B) - 1)
+    # print(x)
+    # x=np.array([-1, 0, 1, 2])
     X = np.vander(x, increasing=True)
+    # print(X)
     y = np.multiply([eval_(A, x) for x in x], [eval_(B, x) for x in x])
     return np.linalg.solve(X, y)
 
 
-def f_(A, B):
+def mult_2(A, B):
     """
-    convert represent Polynomials
+    multiply to Polynomials that represent by vector of values
+        for example: Polynomial A= 1-2x+3x^2 => A=[1,-2,3]
+                     Polynomial B= -4+2x     => B=[-4,2]
+                     Polynomial C= A*B= -4+10x-16x^2+6^3 and the return will be: [ -4  10 -16   6]
 
-    @param:
+    :param:
         @A,B vectors that represent Polynomials
+
+    :return:
+        vector of values that represent Polynomial C= A*B
 
     :complexity: O(n^2)
     """
     C = []
-    n = max(len(A), len(B)) - 1
-    for a, b, i in zip(A, B, range(n)):
-        pass
+    for l in range(len(A) + len(B) - 1):
+        c = 0
+        # print(f'---------- iter {l} -------------------')
+        for i, j in zip(range(min(l, len(A) - 1), -1, -1), range(max(0, l - len(A) + 1), min(l + 1, len(B)))):
+            # print(f'i={i},j={j}')
+            c += A[i] * B[j]
+        C.append(c)
+
+    return C
 
 
 if __name__ == '__main__':
-    A, B = np.array([1, 2, 0]), np.array([-2, -2, 1])
-    f(A, B)
-    f_([2, -1, 0, 2], [1, 0, -2, 1])
+    A, B = np.array([0, 0, 0, 1]), np.array([1, 2, 3, 4, 5])
+    C, D = np.array([0, 1, 2, 3, 4, 5, 6]), np.array([0, 1, 2, 3, 4, 5, 6])
+    E, F = np.array([1, 2, 0]), np.array([-2, -2, 1])
+    G, T = np.array([3]), np.array([5])
+    X, Y = np.array([3]), np.array([1, 2, 3, 4, 5])
+    V, U = np.array([1, 2, 3, 4, 5]), np.array([4])
+    I, J = np.array([1324, 25, 3543, 443, 534]), np.array([43, 45, 6, 5, 5])
+    P, Q = np.array([1, -2, 3]), np.array([-4, 2])
+    # [1, 2, 0],[-2, -2, 1],[-1, 0, 1, 2],[-2. -6. -3.  2.]
+
+    print(np.round(mult_2(P, Q), 5))
+
+    print(np.array_equal(np.round(mult_(A, B), 5), np.round(mult_2(A, B), 5)))
+    print(np.array_equal(np.round(mult_(C, D), 2), np.round(mult_2(C, D), 2)))
+    print(np.array_equal(np.round(mult_(E, F), 5), np.round(mult_2(E, F), 5)))
+    print(np.array_equal(np.round(mult_(G, T), 5), np.round(mult_2(G, T), 5)))
+    print(np.array_equal(np.round(mult_(X, Y), 5), np.round(mult_2(X, Y), 5)))
+    print(np.array_equal(np.round(mult_(V, U), 5), np.round(mult_2(V, U), 5)))
+    print(np.array_equal(np.round(mult_(I, J), 5), np.round(mult_2(I, J), 5)))
+    print(np.array_equal(np.round(mult_(P, Q), 5), np.round(mult_2(P, Q), 5)))
