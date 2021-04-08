59bcd5f20d0c7620f8df8febffcfc796221787e5
bb <bezalelcohen1@gmail.com>
2021-04-08 23:04:54 +0300

    main

--- temp_module/ido.py
+++ temp_module/ido.py
@@ -9,32 +9,39 @@ def f(x):
     return x ** 2 - 17
 
 
-def ido(func, x0, x1, landa=1e-7, epsilon=1e-7, max_iter=100):
+def ido(func, x0, x1, landa=1e-3, epsilon=1e-3, max_iter=100):
     x_history, f_history = [x1], [f(x1)]
+    x0, x1 = np.float128(x0), np.float128(x1)
     # x0, x1 = np.float128, np.float128
     for i in range(max_iter):
         df = (f(x1) - f(x0)) / (x1 - x0)
 
         # if df
-        # try:
-        x0, x1 = x1, x0 - f(x0) / df
-        x_history.append(x1), f_history.append(f(x1))
-        # raise
+        try:
+            x0, x1 = x1, x0 - f(x0) / df  # df
+            x_history.append(x1), f_history.append(f(x1))
+        except ZeroDivisionError:
+            print('choose other points')
+            return
+
         # print(x1)
         if np.abs(f(x1)) < landa or np.abs(x1 - x0) < epsilon:
             return x1, x_history, f_history
 
 
 if __name__ == '__main__':
-    a = np.arange(-10, 10)
+    points = np.linspace(-10, 10, num=1000)
     np.vectorize(f)
 
     x, x_history, f_history = ido(f, 1, 2)
     print(f(x))
 
     plt.figure(figsize=(8, 8))
-    plt.plot(range(20), f(a))
+    plt.plot(range(points.shape[0]), f(points))
+    plt.legend('f(x) graph')
     plt.scatter(x_history, f_history, marker='*', linewidths=3, color='r')
+    print(x_history)
+
     # plt.scatter(len(f_history), f_history,marker='*', linewidths=1)
     plt.xlabel('x')
     plt.ylabel('f(x)')
