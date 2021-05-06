dacb2b3e347f6937c2d4b35347a501af9fb0bbea
bb <bezalelcohen1@gmail.com>
2021-05-06 17:18:29 +0300

    c

--- Algorithms/temp_module/joni.py
+++ Algorithms/temp_module/joni.py
@@ -4,7 +4,6 @@ authors:
     Yehonatan Deri, 209173988
 """
 import numpy as np
-import tkinter
 import sympy as sp
 import matplotlib.pyplot as plt
 
@@ -64,15 +63,15 @@ def calcZ(h, u, v):
 
 def calcS(x, h, z, c, d):
     # S
-    var = sp.symbols('x')
+    var = sp.symbols('var')
     S = z[:-1] / (6 * h) * (x[1:] - var) ** 3 + z[1:] / (6 * h) * (var - x[:-1]) ** 3 + c * (var - x[:-1]) + d * \
         (x[1:] - var)
-    return S, var
+    return S
 
 
 def pointMapping(S, x):
     # map function
-    func = [sp.lambdify(s.free_symbols, s, 'numpy') for s in S]
+    func = [sp.lambdify(list(s.free_symbols), s, 'numpy') for s in S]
     map_points = lambda x0: np.searchsorted(x, x0) - 1 if x0 != x[0] else 0
     splines = np.vectorize(lambda x0: func[map_points(x0)](x0))
     return splines
@@ -83,12 +82,12 @@ def cubicSplain(x, y):
     u, v = rowReduction(u, v, h)
     z = calcZ(h, u, v)
     c, d = calcCD(y, h, z)
-    S, var = calcS(x, h, z, c, d)
+    S = calcS(x, h, z, c, d)
     sMap = pointMapping(S, x)
     return sMap
 
 
-def cubic_spline4_matrix(x, y):
+def cubicSplineMatrix(x, y):
     """
     calculate the coefficients for cubic spline
 
@@ -118,43 +117,33 @@ def cubic_spline4_matrix(x, y):
 
     # calculate the coefficients
     coeff = np.linalg.solve(M, A).reshape((-1, 4))
-    x = sp.symbols('x')
-    S = coeff[:, 0] * x ** 3 + coeff[:, 1] * x ** 2 + coeff[:, 2] * x + coeff[:, 3]
+    var = sp.symbols('var')
+    S = coeff[:, 0] * var ** 3 + coeff[:, 1] * var ** 2 + coeff[:, 2] * var + coeff[:, 3]
 
     # return function
     return pointMapping(S, x)
 
 
 if __name__ == '__main__':
-    # example from juda1
-    # x = np.array([1, 9, 4], dtype=np.float64)
-    # y = np.array([1, 3, 2], dtype=np.float64)
-    # example from juda1
-    # x = np.array([0, 1, 2, 3], dtype=np.float64)
-    # y = np.array([0, 1, 0, -1], dtype=np.float64)
-    # exmaple from some website
-    # x = np.array([0.9, 1.3, 1.9, 2.1], dtype=np.float64)
-    # y = np.array([1.3, 1.5, 1.85, 2.1], dtype=np.float64)
-
+    # get splines
     sizes = np.array([2, 4, 6, 12])
     X = [np.linspace(-6, 6, num=size + 1) for size in sizes]
     Y = [f(x) for x in X]
     splines = [cubicSplain(x, y) for x, y in zip(X, Y)]
+    splines_mat = [cubicSplineMatrix(x, y) for x, y in zip(X, Y)]
 
     # plot
-    plt.style.use('seaborn-bright')
-    z = np.linspace(-6, 6, 1000)
-    fig = plt.figure(figsize=(14, 10))
-    for x, y, spline, i in zip(X, Y, splines, range(221, 225)):
+    plt.style.use('seaborn')
+    z = np.linspace(-6, 6, 1000, dtype=np.float64)
+    fig = plt.figure(figsize=(16, 12))
+    for x, y, spline, spline_mat, i in zip(X, Y, splines, splines_mat, range(221, 225)):
         fig_i = fig.add_subplot(i)
-        fig_i.plot(z, f(z), c='k', alpha=1, label='f(x)')
-        fig_i.plot(z, spline(z), c='b', alpha=0.7, label='spline')
+        fig_i.plot(z, f(z), c='b', alpha=1, label='f(x)')
+        fig_i.plot(z, spline(z), c='g', alpha=0.4, linewidth=8, label='spline')
+        fig_i.plot(z, spline_mat(z), c='k', alpha=1, label='spline mat')
         fig_i.scatter(x, spline(x), marker='o', c='r', label='spline points')
-        fig_i.legend(loc='lower left')
+        fig_i.legend(loc='lower left', fancybox=True, borderaxespad=0, prop={"size": 9})
         fig_i.set_title(f'{len(x) - 1} splines')
         fig_i.set_xlabel('x')
         fig_i.set_ylabel('f(x)')
-
     plt.show()
-    # dark_background,seaborn-bright,grayscale,ggplot,fivethirtyeight,bmh,seaborn-poster
-    # darkorange
